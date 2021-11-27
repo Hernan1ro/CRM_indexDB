4e50b38cbb9ce123fdb4a3c4ddbecb1e70a4406b
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-26 23:22:21 -0500

    Creating the data base for the app

---
+++ js/app.js
@@ -0,0 +1,33 @@
+(function () {
+  let DB;
+  document.addEventListener("DOMContentLoaded", () => {
+    crearDB();
+  });
+  //Crear base de datos IndexedDB
+  function crearDB() {
+    const crearDB = window.indexedDB.open(["crm", 1]);
+    crearDB.onerror = function () {
+      console.log("Hubo un error");
+    };
+    crearDB.onsuccess = function () {
+      DB = crearDB.result;
+      console.log(DB);
+    };
+
+    //Crear el schema de la DB
+    crearDB.onupgradeneeded = function (e) {
+      const db = e.target.result;
+      const objectStorage = db.objectStorage("crm", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+
+      objectStorage.createIndex("nombre", "nombre", { unique: false });
+      objectStorage.createIndex("email", "email", { unique: true });
+      objectStorage.createIndex("telefono", "telefono", { unique: false });
+      objectStorage.createIndex("empresa", "empresa", { unique: false });
+      objectStorage.createIndex("id", "id", { unique: true });
+      console.log("Lista creada");
+    };
+  }
+})();
