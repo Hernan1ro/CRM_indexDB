128637ea3682c9e92dd78feb275d8b33ae6dc2b8
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-13 16:04:06 -0500

    testing the edit functionallity

--- cypress/integration/app-functionallity-test.spec.js
+++ cypress/integration/app-functionallity-test.spec.js
@@ -8,19 +8,26 @@ function fillForm(name, email, phone, company) {
     cy.get('[data-cy="btn-submit"]').click();
   });
 }
+function editForm(name, phone, company) {
+  cy.get('[id="nombre"]').clear().type(name);
+  cy.get('[id="telefono"]').clear().type(phone);
+  cy.get('[id="empresa"]').clear().type(company);
+  cy.get('[data-cy="btn-submit"]').click();
+}
 
-describe("It makes a test the whole app CRM", function () {
+describe("It makes a test to the whole app CRM", function () {
   it("Visit the web app", function () {
-    cy.visit("http://127.0.0.1:5500/");
+    cy.visit("/index.html");
   });
   it("Move to the form section", function () {
     cy.get("[data-cy='nuevo-cliente']").click();
   });
+  // Llena el formulario con la información de 3 usuarios
   fillForm(
     "Hernán David Mercado Fernández",
     "hernandmf@gmail.com",
     "234235235",
-    "C.I. Técnicas Baltime de Colombia S.A."
+    "Professional Freelance"
   );
   it("Move to the form section", function () {
     cy.get("[data-cy='nuevo-cliente']").click();
@@ -30,4 +37,12 @@ describe("It makes a test the whole app CRM", function () {
     cy.get("[data-cy='nuevo-cliente']").click();
   });
   fillForm("Jeff Bezos", "jeffbezos@blueorigin.com", "346534", "Blue Origin");
+
+  it("Edit the client information", () => {
+    setTimeout(() => {
+      cy.get("[data-cy='editar']").first().click();
+      cy.get('[id="nombre"]').clear().type("Hernán Mercado");
+      editForm("Hernán Mercado", "12341234", "Looking for a company");
+    }, 5000);
+  });
 });
