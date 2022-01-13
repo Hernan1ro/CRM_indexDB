3ee79ba414145df3b66628b8c15df64f13d8d7e9
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-13 14:00:41 -0500

    apply form test

--- cypress/integration/app-functionallity-test.spec.js
+++ cypress/integration/app-functionallity-test.spec.js
@@ -1,3 +1,14 @@
+function fillForm(name, email, phone, company) {
+  return it("Fill the form", function () {
+    cy.get('[id="nombre"]').type(name);
+    cy.get('[id="email"]').type(email);
+    cy.get('[id="telefono"]').type(phone);
+    cy.get('[id="empresa"]').type(company);
+
+    cy.get('[data-cy="btn-submit"]').click();
+  });
+}
+
 describe("It makes a test the whole app CRM", function () {
   it("Visit the web app", function () {
     cy.visit("http://127.0.0.1:5500/");
@@ -5,12 +16,18 @@ describe("It makes a test the whole app CRM", function () {
   it("Move to the form section", function () {
     cy.get("[data-cy='nuevo-cliente']").click();
   });
-  it("Fill the form", function () {
-    cy.get('[id="nombre"]').type("Hernán David Mercado Fernández");
-    cy.get('[id="email"]').type("hernandmf@gmail.com");
-    cy.get('[id="telefono"]').type("234235235");
-    cy.get('[id="empresa"]').type("C.I. Técnicas Baltime de Colombia S.A.");
-
-    cy.get('[data-cy="btn-submit"]').click();
+  fillForm(
+    "Hernán David Mercado Fernández",
+    "hernandmf@gmail.com",
+    "234235235",
+    "C.I. Técnicas Baltime de Colombia S.A."
+  );
+  it("Move to the form section", function () {
+    cy.get("[data-cy='nuevo-cliente']").click();
   });
+  fillForm("Elon Musk", "elonmusk@spacex.com", "4563456", "Spacex");
+  it("Move to the form section", function () {
+    cy.get("[data-cy='nuevo-cliente']").click();
+  });
+  fillForm("Jeff Bezos", "jeffbezos@blueorigin.com", "346534", "Blue Origin");
 });
--- js/app.js
+++ js/app.js
@@ -83,8 +83,8 @@
                   <p class="text-gray-600">${empresa}</p>
               </td>
               <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
-                  <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                  <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+                  <a data-cy="editar" href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                  <a data-cy="eliminar" href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
               </td>
           </tr>
           `;
